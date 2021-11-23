d57b4f8c9c576c20b70794d651d2dd85527b3451
l-gorman <lgorman@turing.ac.uk>
2021-11-23 20:07:01 +0000

    adding ability to fetch project info

--- routes/projectData.js
+++ routes/projectData.js
@@ -7,13 +7,33 @@ router.options("*", cors());
 
 const auth = require('../validation/checkAccess')
 
+const projectData = require('../models/projectData')
+
 const data = require("../models/data");
 
 
 router.post("/", auth, async (req, res) => {
+
+    console.log("Reached project Data endpoint")
+    // res.send(req.user.information)
+
+    if (req.user.information.user.roles.projectManager.includes(req.body.projectName) === false &
+        req.user.information.user.roles.analyst.includes(req.body.formName) === false
+    ) {
+        return res.status(400).send("Unauthorized")
+    }
+
     try {
 
+        const projectInfo = await projectData.findOne({ "formID": req.body.formName, "projectID": req.body.projectName })
+
+        if (!projectInfo) return res.status(400).send("No project with those details found")
+
+        return res.status(200).send(projectInfo)
+
     } catch (err) {
+        return res.status(400).send(err)
+
 
     }
 })
